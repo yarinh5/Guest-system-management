f18ab980a0ecacdc4f62b373bb408c460cca053d
Yarin Hazan <87584981+yarinh5@users.noreply.github.com>
2023-07-31 16:35:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-# פונקציה להוספת יחיד חדש למערכת
-def add_individual(name, age, id_number):
-    with open("data.txt", "a") as file:
-        file.write(f"{name},{age},{id_number}\n")
+from datetime import datetime
 
-# פונקציה להצגת רשימת כל היחידים במערכת
-def show_all_individuals():
-    with open("data.txt", "r") as file:
-        for line in file:
-            name, age, id_number = line.strip().split(",")
-            print(f"Name: {name}, Age: {age}, ID: {id_number}")
+class Individual:
+    def __init__(self, name, age, id_number):
+        self.name = name
+        self.age = age
+        self.id_number = id_number
 
-# פונקציה לספירת כמות האורחים במערכת
-def count_individuals():
-    with open("data.txt", "r") as file:
-        return len(file.readlines())
+class GuestManagementSystem:
+    def __init__(self):
+        self.individuals = []
+
+    def add_individual(self, name, age, id_number):
+        individual = Individual(name, age, id_number)
+        self.individuals.append(individual)
+
+    def show_all_individuals(self):
+        for individual in self.individuals:
+            print(f"Name: {individual.name}, Age: {individual.age}, ID: {individual.id_number}")
+
+    def count_individuals(self):
+        return len(self.individuals)
 
 def main():
+    guest_system = GuestManagementSystem()
+
     while True:
         print("\nChoose an option:")
         print("1. Add Individual")
@@ -29,15 +37,15 @@ def main():
             name = input("Enter name: ")
             age = int(input("Enter age: "))
             id_number = input("Enter ID number: ")
-            add_individual(name, age, id_number)
+            guest_system.add_individual(name, age, id_number)
             print("Individual added successfully!")
 
         elif choice == "2":
             print("\nAll Individuals:")
-            show_all_individuals()
+            guest_system.show_all_individuals()
 
         elif choice == "3":
-            total_individuals = count_individuals()
+            total_individuals = guest_system.count_individuals()
             print(f"\nTotal Individuals: {total_individuals}")
 
         elif choice == "4":
